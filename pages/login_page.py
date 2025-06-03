eaee885163e79e171c4874cf9e70380145787b58
Zoe Lyon <zoe@snapehealth.com>
2025-06-02 21:18:02 -0400

    login errors test

--- pages/login_page.py
+++ pages/login_page.py
@@ -6,17 +6,35 @@ class LoginPage(BasePage):
     def __init__(self, page):
         super().__init__(page)
 
-    def login_header(self):
+    def verify_login_header(self):
         assert self.page.locator(".login_logo").is_visible()
 
-    def usernames_list(self):
+    def verify_usernames_list(self):
         username_box = self.page.locator(".login_credentials")
         assert username_box.get_by_role("heading", name="Accepted usernames are:").is_visible()
         accepted_usernames = ["standard_user", "locked_out_user", "problem_user", "performance_glitch_user", "error_user", "visual_user"]
         for username in accepted_usernames:
             assert username_box.get_by_text(username).is_visible(), f"Username not found: {username}"
 
-    def passwords_list(self):
+    def verify_passwords_list(self):
         password_box = self.page.locator(".login_password")
         assert password_box.get_by_role("heading", name= "Password for all users:").is_visible()
         assert password_box.get_by_text("secret_sauce").is_visible()
+
+    def verify_login_username_errors(self):
+
+        login_errors = [
+            ("locked_out_user", "Epic sadface: Sorry, this user has been locked out."),
+            ("problem_user", "Epic sadface: Unexpected issue with the login."),
+            ("invalid_user", "Epic sadface: Username and password do not match any user."),
+        ]
+
+        for username, expected_error in login_errors:
+            self.page.fill("#user-name", username)
+            self.page.fill("#password", "secret_sauce")
+            self.page.click("#login-button")
+
+            error_text = self.page.locator("[data-test='error']").text_content().strip()
+            assert error_text == expected_error, f"For {username}, expected: '{expected_error}', but got: '{error_text}'"
+
+            self.page.reload()
--- tests/test_login_page.py
+++ tests/test_login_page.py
@@ -7,15 +7,19 @@ from pages.login_page import LoginPage
 def test_login_page_header(page):
     login_page = LoginPage(page)
     login_page.go_to()
-    login_page.login_header()
+    login_page.verify_login_header()
 
 def test_login_page_usernames(page):
     login_page = LoginPage(page)
     login_page.go_to()
-    login_page.usernames_list()
+    login_page.verify_usernames_list()
 
 def test_login_page_passwords(page):
     login_page = LoginPage(page)
     login_page.go_to()
-    login_page.passwords_list()
+    login_page.verify_passwords_list()
 
+def test_login_page_errors(page):
+    login_page = LoginPage(page)
+    login_page.go_to()
+    login_page.verify_login_username_errors()
